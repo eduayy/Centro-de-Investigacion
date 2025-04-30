f69d76dc73975915ae98fb5a62938b143eb3b342
eduayy <cesr031223@gmail.com>
2025-04-29 23:18:37 -0600

    popup of dashboard implemented

---
+++ src/components/Dashboard/Dasboard.jsx
@@ -0,0 +1,12 @@
+import "./Dashboard.css"; 
+
+function Dashboard() {
+  return (
+    <div className="dashboard">
+      <h1>Dashboard</h1>
+      <p>Welcome to the Researcher Dashboard!</p>
+    </div>
+  );
+}
+
+export default Dashboard;
